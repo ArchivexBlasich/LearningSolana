c6ec67924ffabd126ccc0c2369a4907927c3a065
ArchivexBlasich <fblasich0@gmail.com>
2025-03-07 17:33:24 -0300

    generate-keypair

---
+++ generate-keypair/generate-keypair.ts
@@ -0,0 +1,25 @@
+/* import { Keypair } from "@solana/web3.js";
+const keypair = Keypair.generate();
+console.log(keypair.publicKey.toBase58());
+console.log(`✅ Generated keypair!`); */
+
+
+
+/* import { Keypair } from "@solana/web3.js";
+
+const keypair = Keypair.generate();
+
+console.log(`The public key is: `, keypair.publicKey.toBase58());
+console.log(`The secret key is: `, keypair.secretKey);
+console.log(`✅ Finished!`); */
+
+
+
+import "dotenv/config";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+console.log(keypair.secretKey)
+console.log(
+  `✅ Finished! We've loaded our secret key securely, using an env file!`,
+);
